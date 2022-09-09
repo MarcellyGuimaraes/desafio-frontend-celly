82c468556d5615f7f8da5b0e18c8f7d1719182d0
IsisG13 <isissouzaguimaraes@gmail.com>
2022-09-08 21:14:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center bg-gradient-to-b from-orange-500 to-yellow-400">
+      <h1 className="font-bold text-4xl text-white mb-8">Previsão do tempo</h1>
+      <input type="text" placeholder="Insira aqui o nome da cidade" />
+
+      <div className="flex justify-center">
+        <h2 className="font-bold text-2xl text-white m-4">Capitais</h2>
+        <table className="content-center">
+          <thead>
+            <tr>
+              <th className="font-thin">Min</th>
+              <th className="font-thin">Máx</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>18°</td>
+              <td>27°</td>
+              <td>Rio de Janeiro</td>
+            </tr>
+            <tr>
+              <td>18°</td>
+              <td>27°</td>
+              <td>Rio de Janeiro</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

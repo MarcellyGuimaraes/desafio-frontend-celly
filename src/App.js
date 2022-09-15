e85ee97a9d51ddcd73f3a8c831c0f16bf6bd4773
IsisG13 <isissouzaguimaraes@gmail.com>
2022-09-15 20:00:03 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,61 +1,50 @@
-import './App.css'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 function App() {
-  const handleClick = () => {
-    const element = document.querySelector('#test')
-    element.classList.toggle('hidden')
+  const [location, setLocation] = useState(false)
+  const [weather, setWeather] = useState(false)
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get(
+      'http://api.openweathermap.org/data/2.5/weather',
+      {
+        params: {
+          lat: lat,
+          lon: long,
+          appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+          lang: 'pt',
+          units: 'metric',
+        },
+      },
+    )
+    setWeather(res.data)
   }
-  return (
-    <div
-      className="flex flex-col justify-center items-center 
-        bg-gradient-to-b from-orange-500 to-yellow-400 w-screen h-screen"
-    >
-      <h1 onClick={handleClick} className="font-bold text-6xl text-white mb-8">
-        Previsão do tempoaaaaaaaaaaaas
-      </h1>
-      <div id="test" className="bg-slate-100 w-fit h-48 p-9 m-14">
-        <span>Niteroi, RJ - Brasil</span>
-        <h1>20°C Nublado</h1>
-        <p>
-          <span>16°C</span> <span>16°C</span> <span>Sensação 19°C</span>
-        </p>
-        <p>
-          <span>Vento 18km/h</span> <span>Humidade 89%</span>
-        </p>
-      </div>
-      <input
-        type="text"
-        className="w-96 h-14 px-5"
-        placeholder="Insira aqui o nome da cidade"
-      />
 
-      <div className="">
-        <h2 className="m-4 w-96 text-left text-3xl font-bold text-white">
-          Capitais
-        </h2>
-        <table className="content-center">
-          <thead>
-            <tr>
-              <th className="font-thin">Min</th>
-              <th className="font-thin">Máx</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>18°</td>
-              <td>27°</td>
-              <td>Rio de Janeiro</td>
-            </tr>
-            <tr>
-              <td>18°</td>
-              <td>27°</td>
-              <td>Rio de Janeiro</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  )
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude)
+      setLocation(true)
+    })
+  }, [])
+
+  if (location === false) {
+    return <>Você precisa habilitar a localização no browser o/</>
+  } else {
+    return (
+      <>
+        <h3>
+          Clima nas suas Coordenadas ({weather['weather'][0]['description']})
+        </h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: {weather['main']['temp']}°</li>
+          <li>Temperatura máxima: {weather['main']['temp_max']}°</li>
+          <li>Temperatura minima: {weather['main']['temp_min']}°</li>
+          <li>Pressão: {weather['main']['pressure']} hpa</li>
+          <li>Humidade: {weather['main']['humidity']}%</li>
+        </ul>
+      </>
+    )
+  }
 }
-
 export default App

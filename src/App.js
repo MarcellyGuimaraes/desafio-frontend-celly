a41fb61752d7969b7f9e53a69c29e9d7976628ad
IsisG13 <isissouzaguimaraes@gmail.com>
2022-09-15 19:49:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="text-center bg-gradient-to-b from-orange-500 to-yellow-400">
-      <h1 className="font-bold text-4xl text-white mb-8">Previsão do tempo</h1>
-      <input type="text" placeholder="Insira aqui o nome da cidade" />
+  fetch('https://api.hgbrasil.com/weather?key=ee302ce2')
+    .then((response) => response.json())
+    .then((data) => {
+      //Aqui iremos trabalhar com o JSON
+      data.forEach((cities) => {
+        // Log de cada nome de filme
+        console.log(cities.city)
+      })
+    })
+    .catch((err) => {
+      //Fazer algo com os erros aqui
+      console.log(err)
+    })
 
-      <div className="flex justify-center">
-        <h2 className="font-bold text-2xl text-white m-4">Capitais</h2>
+  const handleClick = () => {
+    const element = document.querySelector('#test')
+    element.classList.toggle('hidden')
+  }
+  return (
+    <div
+      className="flex flex-col justify-center items-center 
+        bg-gradient-to-b from-orange-500 to-yellow-400 w-screen h-screen"
+    >
+      <h1 onClick={handleClick} className="font-bold text-6xl text-white mb-8">
+        Previsão do tempo
+      </h1>
+      <div id="test" className="bg-slate-100 w-fit h-48 p-9 m-14">
+        <span>Niteroi, RJ - Brasil</span>
+        <h1>20°C Nublado</h1>
+        <p>
+          <span>16°C</span> <span>16°C</span> <span>Sensação 19°C</span>
+        </p>
+        <p>
+          <span>Vento 18km/h</span> <span>Humidade 89%</span>
+        </p>
+      </div>
+      <input
+        type="text"
+        className="w-96 h-14 px-5"
+        placeholder="Insira aqui o nome da cidade"
+      />
+
+      <div className="">
+        <h2 className="m-4 w-96 text-left text-3xl font-bold text-white">
+          Capitais
+        </h2>
         <table className="content-center">
           <thead>
             <tr>

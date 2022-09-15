a69b791a4662b4db1d888f9ae23adc1c9cdf918f
IsisG13 <isissouzaguimaraes@gmail.com>
2022-09-15 20:14:04 -0300

    test

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   const [weather, setWeather] = useState(false)
 
   let getWeather = async (lat, long) => {
-    let res = await axios.get(
-      'http://api.openweathermap.org/data/2.5/weather',
-      {
-        params: {
-          lat: lat,
-          lon: long,
-          appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
-          lang: 'pt',
-          units: 'metric',
-        },
+    let res = await axios.get('http://api.openweathermap.org/weather', {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric',
       },
-    )
+    })
     setWeather(res.data)
   }
 

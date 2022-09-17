7597a027be5afd6ce6211fddaa61f4ec686c31a9
IsisG13 <isissouzaguimaraes@gmail.com>
2022-09-17 00:14:50 -0300

    arrumando card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TableWeather from './Components/TableWeather'
 import { getWeather } from './fetchApiFunc'
-import IconClose from './Components/Icon'
+import { IconClose, IconDown, IconUp } from './Components/Icon'
 import SearchInput from './Components/SearchInput'
 
 export const handleClick = () => {
@@ -44,12 +44,40 @@ function App() {
         <div id="test" className="m-14 h-48 w-96 bg-slate-100 p-9">
           <IconClose onClick={handleClick} />
 
-          <span>{weather.name}</span>
+          <span className="text-xs text-gray-600">{weather.name}</span>
 
-          <h3 className="text-4xl font-semibold">
-            {Math.floor(weather.main.temp)}°C{' '}
+          <h3 className="text-2xl font-semibold">
+            {Math.floor(weather.main.temp)}°C{' - '}
             {weather['weather'][0]['description']}
           </h3>
+
+          <table class="table">
+            <tr>
+              <td>
+                <IconDown /> {Math.floor(weather.main.temp_min)}°C
+              </td>
+              <td>
+                <IconUp /> {Math.floor(weather.main.temp_max)}°C
+              </td>
+              <td>
+                {' '}
+                <p class="text-xs text-gray-600">Sensação</p>{' '}
+                {Math.floor(weather.main.feels_like)}°C
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {' '}
+                <p class="text-xs text-gray-600">Vento</p>{' '}
+                {Math.floor(weather.wind.speed)}km/h
+              </td>
+              <td>
+                {' '}
+                <p class="text-xs text-gray-600">Humidade</p>{' '}
+                {Math.floor(weather.main.humidity)}%
+              </td>
+            </tr>
+          </table>
         </div>
 
         <SearchInput />
--- src/Components/Icon/index.jsx
+++ src/Components/Icon/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { handleClick } from '../../App'
 
-const IconClose = () => {
+export const IconClose = () => {
   return (
     <div>
       <svg
@@ -10,7 +10,7 @@ const IconClose = () => {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="w-6 h-6"
+        className="w-5 h-5 absolute p ml-80 -mt-6"
         onClick={handleClick}
       >
         <path
@@ -23,4 +23,40 @@ const IconClose = () => {
   )
 }
 
-export default IconClose
+export const IconUp = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-5 h-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+      />
+    </svg>
+  )
+}
+
+export const IconDown = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-5 h-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+      />
+    </svg>
+  )
+}
